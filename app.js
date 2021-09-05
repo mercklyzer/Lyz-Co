f365f3373e714851087f4fd46a4d5f3830d1c76e
Lyzer Merck Bautista <mercklyzer@gmail.com>
2021-09-05 17:41:54 +0800

    Finish intro overlay

---
+++ app.js
@@ -0,0 +1,29 @@
+let tl = gsap.timeline()
+
+tl.from('.header-1', {
+    yPercent:-100,
+    ease:"power3.out",
+    duration: 0.3
+}, 1)
+.to('.header-1', {
+    yPercent:100,
+    ease:"power3.out",
+    duration: 0.5
+}, 4)
+.from('.header-2', {
+    yPercent: -100,
+    ease:"power3.out",
+    duration: 0.3
+}, 4.5)
+.to('.header-2', {
+    yPercent: 100,
+    ease:"power3.out",
+    duration: 0.5
+}, 6)
+.to('.intro-overlay', {
+    opacity: 0
+})
+
+
+
+console.log("working")
