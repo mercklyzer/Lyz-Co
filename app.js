892ba8c2ee5316b5337a4ec1ca9974333a75045d
Lyzer Merck Bautista <mercklyzer@gmail.com>
2021-09-05 21:53:23 +0800

    Add animation on hero section

--- app.js
+++ app.js
@@ -22,9 +22,69 @@ tl.from('.header-1', {
 }, 6)
 .to('.intro-overlay', {
     opacity: 0,
-    zIndex: -1
+    zIndex: -1,
+    duration: 0.5
+})
+.from('.top-header-1', {
+    yPercent: 20,
+    opacity: 0,
+})
+.from('.top-header-2', {
+    yPercent: 20,
+    opacity: 0,
+})
+.from('.hero-wrapper img', {
+    yPercent: 10,
+    opacity: 0,
+    zIndex: 2
+})
+.from('.bottom-header', {
+    yPercent: 10,
+    opacity: 0,
 })
 
+ScrollTrigger.defaults({
+
+    markers: true,
+})
+
+gsap.to('.top-header-1', {
+    scrollTrigger: {
+        trigger: '.hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1
+    },
+    yPercent: -100
+})
+gsap.to('.top-header-2', {
+    scrollTrigger: {
+        trigger: '.hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1
+    },
+    yPercent: -100
+})
+gsap.to('.bottom-header', {
+    scrollTrigger: {
+        trigger: '.hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1
+    },
+    yPercent: -100
+})
+gsap.to('.hero-wrapper img', {
+    scrollTrigger: {
+        trigger: '.hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1
+    },
+    scale: 1.2,
+    transformOrigin: '50% 50%'
+})
 
 
 console.log("working")
