661a022cbfdf275b59d08f10088b6f0b5fb154ad
Lyzer Merck Bautista <mercklyzer@gmail.com>
2021-09-06 11:37:39 +0800

    Add horizontal line with animation scrollTrigger

--- app.js
+++ app.js
@@ -1,57 +1,56 @@
 console.clear()
 
-let tl = gsap.timeline()
+// let tl = gsap.timeline()
 
-tl.from('.header-1', {
-    yPercent:-100,
-    ease:"power3.out",
-    duration: 0.3,
-}, 1)
-.to('.header-1', {
-    yPercent:100,
-    ease:"power3.out",
-    duration: 0.5
-}, 4)
-.from('.header-2', {
-    yPercent: -100,
-    ease:"power3.out",
-    duration: 0.3
-}, 4.5)
-.to('.header-2', {
-    yPercent: 100,
-    ease:"power3.out",
-    duration: 0.5
-}, 6)
-.to('.intro-overlay', {
-    opacity: 0,
-    zIndex: -1,
-    duration: 0.5,
-    onStart: () => {
-        window.scrollTo(0,0);
-        document.querySelector('body').style.overflow = 'auto';
-    }
-})
-.from('.hero-wrapper img', {
-    yPercent: 10,
-    opacity: 0,
-    zIndex: 2
-}, 7)
-.from('.top-header-1', {
-    y: '20%',
-    opacity: 0,
-}, 7.3)
-.from('.top-header-2', {
-    y: '20%',
-    opacity: 0,
-}, 7.6)
-
-.from('.bottom-header', {
-    yPercent: 10,
-    opacity: 0,
-}, 7.9)
+// tl.from('.header-1', {
+//     yPercent:-100,
+//     ease:"power3.out",
+//     duration: 0.3,
+// }, 1)
+// .to('.header-1', {
+//     yPercent:100,
+//     ease:"power3.out",
+//     duration: 0.5
+// }, 4)
+// .from('.header-2', {
+//     yPercent: -100,
+//     ease:"power3.out",
+//     duration: 0.3
+// }, 4.5)
+// .to('.header-2', {
+//     yPercent: 100,
+//     ease:"power3.out",
+//     duration: 0.5
+// }, 6)
+// .to('.intro-overlay', {
+//     opacity: 0,
+//     zIndex: -1,
+//     duration: 0.5,
+//     onStart: () => {
+//         window.scrollTo(0,0);
+//         document.querySelector('body').style.overflow = 'auto';
+//     }
+// })
+// .from('.hero-wrapper img', {
+//     yPercent: 10,
+//     opacity: 0,
+//     zIndex: 2
+// }, 7)
+// .from('.top-header-1', {
+//     y: '20%',
+//     opacity: 0,
+// }, 7.3)
+// .from('.top-header-2', {
+//     y: '20%',
+//     opacity: 0,
+// }, 7.6)
+// .from('.bottom-header', {
+//     yPercent: 10,
+//     opacity: 0,
+// }, 7.9)
 
 ScrollTrigger.defaults({
-    // markers: true
+    markers: true
 })
 
 gsap.to('.top-header-1', {
@@ -90,4 +89,17 @@ gsap.to('.hero-wrapper img', {
     },
     scale: 1.2,
     transformOrigin: '50% 50%'
+})
+
+
+// animations for about section
+gsap.from('.hr', {
+    scrollTrigger: {
+        trigger: '.hr',
+        start: 'top bottom'
+    },
+    width: 0,
+    transformOrigin: 'left center',
+    ease: Power1.easeOut,
+    duration: 2,
 })
