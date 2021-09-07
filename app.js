00746fe5a49fb300f045a46b6c3327925d1f7b02
mercklyzer <mercklyzer@gmail.com>
2021-09-07 11:57:08 +0800

    Add about page for desktop

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 console.clear()
 
-// let tl = gsap.timeline()
+let tl = gsap.timeline()
 
-// tl.from('.header-1', {
-//     yPercent:-100,
-//     ease:"power3.out",
-//     duration: 0.3,
-// }, 1)
-// .to('.header-1', {
-//     yPercent:100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 4)
-// .from('.header-2', {
-//     yPercent: -100,
-//     ease:"power3.out",
-//     duration: 0.3
-// }, 4.5)
-// .to('.header-2', {
-//     yPercent: 100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 6)
-// .to('.intro-overlay', {
-//     opacity: 0,
-//     zIndex: -1,
-//     duration: 0.5,
-//     onStart: () => {
-//         window.scrollTo(0,0);
-//         document.querySelector('body').style.overflow = 'auto';
-//     }
-// })
-// .from('.hero-wrapper img', {
-//     yPercent: 10,
-//     opacity: 0,
-//     zIndex: 2
-// }, 7)
-// .from('.top-header-1', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.3)
-// .from('.top-header-2', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.6)
-// .from('.bottom-header', {
-//     yPercent: 10,
-//     opacity: 0,
-// }, 7.9)
+tl.from('.header-1', {
+    yPercent:-100,
+    ease:"power3.out",
+    duration: 0.3,
+}, 1)
+.to('.header-1', {
+    yPercent:100,
+    ease:"power3.out",
+    duration: 0.5
+}, 4)
+.from('.header-2', {
+    yPercent: -100,
+    ease:"power3.out",
+    duration: 0.3
+}, 4.5)
+.to('.header-2', {
+    yPercent: 100,
+    ease:"power3.out",
+    duration: 0.5
+}, 6)
+.to('.intro-overlay', {
+    opacity: 0,
+    zIndex: -1,
+    duration: 0.5,
+    onStart: () => {
+        window.scrollTo(0,0);
+        document.querySelector('body').style.overflow = 'auto';
+    }
+})
+.from('.hero-wrapper img', {
+    yPercent: 10,
+    opacity: 0,
+    zIndex: 2
+}, 7)
+.from('.top-header-1', {
+    y: '20%',
+    opacity: 0,
+}, 7.3)
+.from('.top-header-2', {
+    y: '20%',
+    opacity: 0,
+}, 7.6)
+.from('.bottom-header', {
+    yPercent: 10,
+    opacity: 0,
+}, 7.9)
 
 ScrollTrigger.defaults({
-    markers: true
+    // markers: true
 })
 
 gsap.to('.top-header-1', {
@@ -103,3 +103,54 @@ gsap.to('.hr-wrapper', {
     ease: Power1.easeOut,
     duration: 2,
 })
+
+gsap.from('.left-col .image-wrapper img', {
+    scrollTrigger: {
+        trigger: '.left-col .image-wrapper img',
+        start: '+=20% bottom',
+    },
+    paddingTop: '100%',
+    duration: 1
+})
+
+gsap.to('.left-col .image-wrapper img', {
+    scrollTrigger: {
+        trigger: '.left-col .image-wrapper img',
+        start: 'center bottom',
+        scrub: 1
+    },
+    scale: 1.2,
+})
+
+gsap.to('.right-col', {
+    scrollTrigger: {
+        trigger: '.right-col',
+        start: 'center bottom',
+        scrub: 1
+    },
+    y: '-10%',
+})
+
+gsap.to('.right-col .image-wrapper', {
+    scrollTrigger: {
+        trigger: '.right-col .image-wrapper img',
+        start: 'center bottom',
+        scrub: 1
+    },
+    y: '-20%',
+})
+
+gsap.to('.right-col .image-wrapper img', {
+    scrollTrigger: {
+        trigger: '.right-col .image-wrapper img',
+        start: 'center bottom',
+        scrub: 1
+    },
+    scale: 1.2,
+})
+
+
+
+
+
+
