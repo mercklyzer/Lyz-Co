2673c823032459950ab749d62eab4f62d0503481
mercklyzer <mercklyzer@gmail.com>
2021-09-07 18:48:54 +0800

    Adjust padding to vw

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 console.clear()
 
-// let tl = gsap.timeline()
+let tl = gsap.timeline()
 
-// tl.from('.header-1', {
-//     yPercent:-100,
-//     ease:"power3.out",
-//     duration: 0.3,
-// }, 1)
-// .to('.header-1', {
-//     yPercent:100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 4)
-// .from('.header-2', {
-//     yPercent: -100,
-//     ease:"power3.out",
-//     duration: 0.3
-// }, 4.5)
-// .to('.header-2', {
-//     yPercent: 100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 6)
-// .to('.intro-overlay', {
-//     opacity: 0,
-//     zIndex: -1,
-//     duration: 0.5,
-//     onStart: () => {
-//         window.scrollTo(0,0);
-//         document.querySelector('body').style.overflow = 'auto';
-//     }
-// })
-// .from('.hero-wrapper img', {
-//     yPercent: 10,
-//     opacity: 0,
-//     zIndex: 2
-// }, 7)
-// .from('.top-header-1', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.3)
-// .from('.top-header-2', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.6)
-// .from('.bottom-header', {
-//     yPercent: 10,
-//     opacity: 0,
-// }, 7.9)
+tl.from('.header-1', {
+    yPercent:-100,
+    ease:"power3.out",
+    duration: 0.3,
+}, 1)
+.to('.header-1', {
+    yPercent:100,
+    ease:"power3.out",
+    duration: 0.5
+}, 4)
+.from('.header-2', {
+    yPercent: -100,
+    ease:"power3.out",
+    duration: 0.3
+}, 4.5)
+.to('.header-2', {
+    yPercent: 100,
+    ease:"power3.out",
+    duration: 0.5
+}, 6)
+.to('.intro-overlay', {
+    opacity: 0,
+    zIndex: -1,
+    duration: 0.5,
+    onStart: () => {
+        window.scrollTo(0,0);
+        document.querySelector('body').style.overflow = 'auto';
+    },
+})
+.from('.hero-wrapper img', {
+    yPercent: 10,
+    opacity: 0,
+    zIndex: 2
+}, 7)
+.from('.top-header-1', {
+    y: '20%',
+    opacity: 0,
+}, 7.3)
+.from('.top-header-2', {
+    y: '20%',
+    opacity: 0,
+}, 7.6)
+.from('.bottom-header', {
+    yPercent: 10,
+    opacity: 0,
+}, 7.9)
 
 ScrollTrigger.defaults({
     // markers: true
@@ -96,7 +96,8 @@ gsap.to('.hero-wrapper img', {
 gsap.to('.hr-wrapper', {
     scrollTrigger: {
         trigger: '.hr-wrapper',
-        start: 'top bottom'
+        start: 'top 90%',
+        markers: true
     },
     width: '100%',
     transformOrigin: 'left center',
@@ -153,7 +154,8 @@ gsap.to('.right-col .image-wrapper img', {
 gsap.from('.infinity', {
     scrollTrigger: {
         trigger: '.infinity',
-        start: "top center",
+        start: "top 80%",
+        markers: true
     },
     opacity: 0
 })
@@ -161,9 +163,8 @@ gsap.from('.infinity', {
 gsap.to('.infinity .line-1', {
     scrollTrigger: {
         trigger: '.infinity',
-        start: "top center",
+        start: "top 80%",
         scrub: 1,
-        markers: true
     },
     x: '-40%'
 })
@@ -171,7 +172,7 @@ gsap.to('.infinity .line-1', {
 gsap.to('.infinity .line-2', {
     scrollTrigger: {
         trigger: '.infinity',
-        start: 'top center',
+        start: "top 80%",
         scrub: 1
     },
     x: '40%'
