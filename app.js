4ca49aa46077727988950fbd05a12ee4d2254dab
mercklyzer <mercklyzer@gmail.com>
2021-09-09 00:18:29 +0800

    Updated responsiveness

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 console.clear()
 
-// let tl = gsap.timeline()
+let tl = gsap.timeline()
 
-// tl.from('.header-1', {
-//     yPercent:-100,
-//     ease:"power3.out",
-//     duration: 0.3,
-// }, 1)
-// .to('.header-1', {
-//     yPercent:100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 4)
-// .from('.header-2', {
-//     yPercent: -100,
-//     ease:"power3.out",
-//     duration: 0.3
-// }, 4.5)
-// .to('.header-2', {
-//     yPercent: 100,
-//     ease:"power3.out",
-//     duration: 0.5
-// }, 6)
-// .to('.intro-overlay', {
-//     opacity: 0,
-//     zIndex: -1,
-//     duration: 0.5,
-//     onStart: () => {
-//         window.scrollTo(0,0);
-//         document.querySelector('body').style.overflow = 'auto';
-//     },
-// })
-// .from('.hero-wrapper img', {
-//     yPercent: 10,
-//     opacity: 0,
-//     zIndex: 2
-// }, 7)
-// .from('.top-header-1', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.3)
-// .from('.top-header-2', {
-//     y: '20%',
-//     opacity: 0,
-// }, 7.6)
-// .from('.bottom-header', {
-//     yPercent: 10,
-//     opacity: 0,
-// }, 7.9)
+tl.from('.header-1', {
+    yPercent:-100,
+    ease:"power3.out",
+    duration: 0.3,
+}, 1)
+.to('.header-1', {
+    yPercent:100,
+    ease:"power3.out",
+    duration: 0.5
+}, 4)
+.from('.header-2', {
+    yPercent: -100,
+    ease:"power3.out",
+    duration: 0.3
+}, 4.5)
+.to('.header-2', {
+    yPercent: 100,
+    ease:"power3.out",
+    duration: 0.5
+}, 6)
+.to('.intro-overlay', {
+    opacity: 0,
+    zIndex: -1,
+    duration: 0.5,
+    onStart: () => {
+        window.scrollTo(0,0);
+        document.querySelector('body').style.overflow = 'auto';
+    },
+})
+.from('.hero-wrapper img', {
+    yPercent: 10,
+    opacity: 0,
+    zIndex: 2
+}, 7)
+.from('.top-header-1', {
+    y: '20%',
+    opacity: 0,
+}, 7.3)
+.from('.top-header-2', {
+    y: '20%',
+    opacity: 0,
+}, 7.6)
+.from('.bottom-header', {
+    yPercent: 10,
+    opacity: 0,
+}, 7.9)
 
 ScrollTrigger.defaults({
     // markers: true
