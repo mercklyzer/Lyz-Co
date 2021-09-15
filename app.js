aff7a3c009b5ddaa27ef3451a7371e95aa28ef22
mercklyzer <mercklyzer@gmail.com>
2021-09-15 23:26:26 +0800

    Add scroll to

--- app.js
+++ app.js
@@ -446,6 +446,86 @@ const enableContactOverlay = () => {
     close.addEventListener('click', hideContactOverlay)
 }
 
+const openSidebar = () => {
+    gsap.set('body', {overflow: 'hidden'})
+    gsap.set('.section-preview', {
+        display: 'block'
+    })
+
+    let t1 = gsap.timeline()
+
+    t1.to('.sidebar', {
+        left: 0,
+        ease: Circ.easeOut,
+        duration: 0.4
+    })
+    .from('.sidebar > .body', {
+        opacity: 0,
+        y: '20%',
+        duration: 0.4
+    })
+}
+
+const closeSidebar = () => {
+    gsap.set('body', {overflow: 'auto'})
+    gsap.set('.section-preview', {
+        display: 'none'
+    })
+
+    let t1 = gsap.timeline()
+
+    t1.to('.sidebar', {
+        left: '100%',
+        duration: 0.4
+    })
+}
+
+const enableSidebar = () => {
+    document.querySelector('#menu').addEventListener('click', () => {
+        openSidebar();
+    })
+
+    document.querySelector('.sidebar ion-icon').addEventListener('click', () => {
+        closeSidebar();
+    })
+
+    let bacgkroundImages = ['9310bc54', '7daf311a', '8f6da8bb', '48da48c6']
+
+    let sidebarItems = document.querySelectorAll('.sidebar > .body > .item-wrapper > .item-label')
+    sidebarItems.forEach((sidebarItem, i) => {
+
+        sidebarItem.addEventListener('mouseover', () => {
+            gsap.set('.sidebar > .section-preview', {
+                'background-image':`url(./images/${bacgkroundImages[i]}.jpg)`
+            })
+
+            gsap.to('.section-preview', {
+                width: '400px',
+                ease: Expo.easeInOut,
+                duration: 1
+            })
+        })
+
+        sidebarItem.addEventListener('mouseleave', () => {
+            gsap.to('.section-preview', {
+                width: 0,
+                ease: Expo.easeInOut,
+                duration: 1
+            })
+        })
+
+        sidebarItem.addEventListener('click', () => {
+            closeSidebar();
+            moveTo(sidebarItem.getAttribute('destination'));
+        })
+    })
+
+}
+
+const moveTo = (div) => {
+    gsap.to(window, {duration: 1, scrollTo: `#${div}`});
+}
+
 // showIntroOverlay()
 showHero();
 showAbout();
@@ -455,3 +535,4 @@ darken();
 showContact();
 enableCircle();
 enableContactOverlay();
+enableSidebar();
