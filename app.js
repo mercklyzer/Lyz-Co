e5f1ddd77124261fb1dde86fe5851f3c46a2768d
mercklyzer <mercklyzer@gmail.com>
2021-09-16 12:13:12 +0800

    Fix intro overlay glitch

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 console.clear()
 
 const showIntroOverlay = () => {
-    let tl = gsap.timeline()
 
-    tl.from('.header-1', {
-        yPercent:-100,
+
+    let tl = gsap.timeline()
+    tl.to('.header-1', {
+        y:0,
         ease:"power3.out",
-        duration: 0.3,
-    }, 1)
+        duration: 0.3
+    },1)
     .to('.header-1', {
-        yPercent:100,
+        y:'100%',
         ease:"power3.out",
         duration: 0.5
     }, 4)
-    .from('.header-2', {
-        yPercent: -100,
+    .to('.header-2', {
+        y: 0,
         ease:"power3.out",
         duration: 0.3
     }, 4.5)
     .to('.header-2', {
-        yPercent: 100,
+        y: '100%',
         ease:"power3.out",
         duration: 0.5
     }, 6)
